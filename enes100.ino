034e77d7c262f3b071d76e8a59e2fb2a9bbf59ed
Fucheng Li <lfucheng@yahoo.com>
2019-10-15 13:44:06 -0400

    Facing Target

--- enes100.ino
+++ enes100.ino
@@ -1,44 +1,108 @@
 #include "Enes100.h"
 #include "Tank.h"
+#include <math.h>
 
+double x;
+double y;
+double theta;
+double destination_x;
+double destination_y;
+bool right_x;
+bool faceTarget;
+	
 void setup() {
-  Enes100.begin("Aqua Terps", WATER, 3, 8, 9);
-  Tank.begin();
-  Tank.setLeftMotorPWM(255);
-  Tank.setRightMotorPWM(255);
+	Enes100.begin("Aqua Terps", WATER, 3, 8, 9);
+	Tank.begin();
+	Tank.setLeftMotorPWM(255);
+	Tank.setRightMotorPWM(-255);
+	if(Enes100.updateLocation()){
+	  theta = Enes100.location.theta;
+	  x = Enes100.location.x;
+	  y = Enes100.location.y;
+ 	 }
+	destination_x= Enes100.destination.x; 
+	destination_y= Enes100.destination.y; 
+	right_x = false;
+	faceTarget = false;
 }
 
 void loop() {
-  Enes100.updateLocation();
-  Enes100.println(Enes100.location.theta);
-  Enes100.println(Enes100.location.y);
-  Enes100.println(Enes100.location.x);
-  if(Enes100.location.theta > 0.05 && Enes100.location.theta <= 3.14){
-    Tank.setLeftMotorPWM(255);
-    Tank.setRightMotorPWM(-255);
-  }
-  else if(Enes100.location.theta >= -3.14 && Enes100.location.theta < -0.05){
-    Tank.setLeftMotorPWM(-255);
-    Tank.setRightMotorPWM(255);
-  }
-  else{
-    Tank.setLeftMotorPWM(255);
-    Tank.setRightMotorPWM(255);
-  }
-/*  if(Tank.readDistanceSensor(0) <= 0.3 || Tank.readDistanceSensor(2) <= 0.3){
-    Tank.turnOffMotors();
-    if((Tank.readDistanceSensor(0) < Tank.readDistanceSensor(2)) && (Enes100.location.y <= 1.5 && Enes100.location.y >= 0.2)){
-      Tank.setLeftMotorPWM(255);
-      Tank.setRightMotorPWM(-255);
-    }
-  } */
-  if(Enes100.location.x  >= 3.2 && Enes100.location.x <= 3.6){
-    if(Tank.readDistanceSensor(0) <= 0.15 || Tank.readDistanceSensor(2) <= 0.15){
-      Tank.turnOffMotors();
-    }
-    else if(Tank.readDistanceSensor(0) < Tank.readDistanceSensor(2)){
-      Tank.setLeftMotorPWM(255);
-      Tank.setRightMotorPWM(-255);
-    }
-  }
+	right_x = facingRight();
+	if(facingTarget()){
+		Tank.setLeftMotorPWM(0);
+		Tank.setRightMotorPWM(0);
+	}
+	while(fabs(destination_x - x) >0){
+		Tank.setLeftMotorPWM(255);
+		Tank.setRightMotorPWM(255);
+	}
+}
+
+void location(){
+	if(Enes100.updateLocation()){
+	  theta = Enes100.location.theta;
+	  x = Enes100.location.x;
+	  y = Enes100.location.y;
+  	}
+}
+
+bool facingRight(){
+	location();
+	
+	bool right = false;
+	
+	if(Enes100.location.theta > 0.05 && Enes100.location.theta <= 3.14){
+		Tank.setLeftMotorPWM(255);
+		Tank.setRightMotorPWM(-255);
+		
+  	}
+  	else if(Enes100.location.theta >= -3.14 && Enes100.location.theta <= -0.05){
+		Tank.setLeftMotorPWM(-255);
+		Tank.setRightMotorPWM(255);
+  	}
+  	else{
+		Tank.setLeftMotorPWM(0);
+		Tank.setRightMotorPWM(0);
+		right = true;
+  	}
+	
+	return right;
+}
+
+bool facingTarget(){
+	
+	double x_diff = destination_x- x;
+	double y_diff = destination_y- y;
+	
+	double  beta = atan2 (y_diff, x_diff);
+	bool faceTarget = false;
+	
+	Enes100.print("theta");
+	Enes100.println(theta);
+	Enes100.println(beta);
+
+	
+	if(beta >=0){
+		
+		while(fabs(theta - beta) >= 0.05){
+			location();
+			Tank.setLeftMotorPWM(-255);
+			Tank.setRightMotorPWM(255);
+		}
+		faceTarget = true;
+		
+	}else{
+		beta = fabs(beta);
+		while(fabs(theta - beta) >= 0.05){
+			location();
+			
+			Tank.setLeftMotorPWM(255);
+			Tank.setRightMotorPWM(-255);
+		}
+		faceTarget = true;
+		
+	}
+	return faceTarget;
+	
+		
 }
